c437bbe40294b80c968cceee8db51d9c21b1bb57
csakib049 <136784151+csakib049@users.noreply.github.com>
2023-07-27 05:51:36 +0600

    Create  Write a program in C to print the Fibonacci Series using recursion.cpp
    https://www.w3resource.com/c-programming-exercises/recursion/c-recursion-exercise-3.php

---
+++ Write a program in C to print the Fibonacci Series using recursion.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int fibonacci(int n){
+    if(n==0){
+        return 0;
+    }else if(n==1){
+        return 1;
+        
+    }else{
+        return fibonacci(n-1)+fibonacci(n-2);
+    }
+}
+int main(){
+    int fibonacci(int n);
+    int n,i;
+    cout<<"Enter a number : ";
+    cin>>n;
+    for(i=1;i<=n;i++){
+        
+        cout<<" "<<fibonacci(i);
+    }
+    
+    return 0;
+}
